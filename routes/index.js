4c4b40e4c09a4d1ff189af314ec2f6a5ca67049c
nitinmalhotra21 <nitin@shapetrek.com>
2020-04-03 03:10:53 +0530

    ip changed

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,25 @@
 var express = require("express");
 var router = express.Router();
-const geoip = require("geoip-lite");
+const ipInfo = require("ipinfo");
 const UserService = require("../services/user");
+
 /* GET home page. */
 router.get("/", async (req, res, next) => {
   const ipAddress =
     req.headers["ip-address"] ||
     req.headers["x-forwarded-for"] ||
     req.connection.remoteAddress;
-  const geo = (await geoip.lookup(ipAddress)) || {};
-  const { city, country, region, timezone } = geo;
-  let name = req.query.n || "[Your_NAME]";
-  await UserService.newUser({
-    city,
-    country,
-    region,
-    timezone,
-    ipAddress,
-    name
+  await ipInfo(ipAddress, async (err, data) => {
+    if (err) {
+      console.log("Error: ", err.message);
+    }
+    let name = req.query.n || "[Your_NAME]";
+    await UserService.newUser({
+      name,
+      ...data
+    });
+    res.render("index", { name: name });
   });
-  res.render("index", { name: name });
 });
 
 module.exports = router;
--- schema/user.js
+++ schema/user.js
@@ -2,10 +2,14 @@ var mongoose = require("mongoose");
 
 var UserSchema = new mongoose.Schema({
   name: { type: String },
-  ipAddress: { type: String },
+  ip: { type: String },
   city: { type: String },
   country: { type: String },
   region: { type: String },
-  timezone: { type: String }
+  loc: { type: String },
+  hostname: { type: String },
+  org: { type: String },
+  postal: { type: String }
 });
+
 module.exports = mongoose.model("User", UserSchema);
