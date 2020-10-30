22af910de6e7d2f3a6410c518fb09006013f8141
nmcfcnm <nmcfcnm@gmail.com>
2020-10-31 05:28:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+let indexRouter = require('./routes/index');
 
-var app = express();
+let app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
-// catch 404 and forward to error handler
+// catch 404 and forward to error handler.
 app.use(function(req, res, next) {
   next(createError(404));
 });
