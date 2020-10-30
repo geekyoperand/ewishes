6dd54d6638cccd22cd7e784eb16710dfe842cc25
nmcfcnm <nmcfcnm@gmail.com>
2020-10-31 05:28:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 
-var app = express();
+let app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
-// catch 404 and forward to error handler
+// catch 404 and forward to error handler.
 app.use(function(req, res, next) {
   next(createError(404));
 });
