af14441b69e6e3d7a44ef4c49f9d50a44e943628
geekyoperand <52500177+geekyoperand@users.noreply.github.com>
2020-11-27 02:38:20 +0530

    Revert "Update app.js"

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // return and render the error page
+  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
